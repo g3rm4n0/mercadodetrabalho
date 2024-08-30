7e828b4ec491eacb746ec67884388339f3d38b5e
Germano Ventura <og3rm4n0@gmail.com>
2024-08-29 21:32:47 -0300

    Add files via upload

---
+++ Fundamentos/script.js
@@ -0,0 +1,7 @@
+console.log("Olá, Mundo!");
+let preco= prompt("Digite um preço");
+document.write(preco)
+let nome= prompt("Digite seu nome");
+let idade= prompt("Digite sua idade");
+let pronomes= prompt("Digite seus pronomes")
+document.write("Olá",nome," Seja bem-vinde ao site");
