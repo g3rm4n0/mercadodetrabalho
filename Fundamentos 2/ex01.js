9613e60a6b27a6e5afee909724a8a41ef64d822e
Germano Ventura <og3rm4n0@gmail.com>
2024-08-30 21:29:57 -0300

    Add files via upload

---
+++ Fundamentos 2/ex01.js
@@ -0,0 +1,9 @@
+/*Declare duas variáveis chamadas "idade" e "peso",
+atribua valores a elas e imprima os valores no console*/
+var idade=24
+var peso=60
+
+console.log("Olá, você tem "+idade+" anos");
+
+console.log("E o seu peso peso é de "+peso+ "kg")
+
