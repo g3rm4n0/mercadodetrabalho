740fc5a60d951c36473b5d84f1361b4a42f3bbe7
Germano Ventura <og3rm4n0@gmail.com>
2024-09-13 19:06:53 -0300

    Add files via upload

---
+++ Exercício 11/atividade11.js
@@ -0,0 +1,11 @@
+
+var salario= parseFloat(400)
+var menor= salario *0.15 + salario
+var maior= salario + salario*12.5 
+
+if(salario <= 500){
+    console.log("menor")
+}
+else if(salario > salario <= maior ){
+    console.log("")
+}
