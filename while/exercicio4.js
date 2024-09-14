7f24d379d1ab9ca80b9ac949bb9abf5a8f2eed1c
Germano Ventura <og3rm4n0@gmail.com>
2024-09-13 21:29:02 -0300

    Add files via upload

---
+++ while/exercicio4.js
@@ -0,0 +1,11 @@
+//Escreva um programa que usa um loop while para imprimir
+//todos os números ímpares entre 1 e 20.
+
+let n = 1
+
+if (n <=20) {
+    if (n %2 !==0){
+       console.log(n)
+}
+    n++
+}
---
+++ while/exercicio5.js
@@ -0,0 +1,7 @@
+let senha = prompt("Digite sua senha")
+let senhaCorreta = "5665"
+
+while(senha !== senhaCorreta) {
+        senha = prompt('Digite a senha')
+}
+
