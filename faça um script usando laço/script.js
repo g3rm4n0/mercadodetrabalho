c71a8229f6b72cc6079584ceb48ce353c9e7c747
Germano Ventura <og3rm4n0@gmail.com>
2024-09-18 19:07:11 -0300

    Add files via upload

---
+++ faça um script usando laço/script.js
@@ -0,0 +1,6 @@
+//Faça um script que, usando apenas um laço for, exiba as duas
+//colunas a seguir:
+    function exibir(){
+        for(var a=0, b=10 ; a<=10 && b>=0; a++, b--) // 
+        document.write(a+" - "+b+"<br />");
+    }
