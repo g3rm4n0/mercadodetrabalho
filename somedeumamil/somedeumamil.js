7135a0fedca8fdfd461dd4e121a1ecf27ff8b4e8
Germano Ventura <og3rm4n0@gmail.com>
2024-09-18 19:07:45 -0300

    Add files via upload

---
+++ somedeumamil/somedeumamil.js
@@ -0,0 +1,9 @@
+//Crie um programa que some de 1 a 1000 utilizando for
+function somar(){
+    var soma=0;
+    var count;
+    for(count=1 ; count<=1000 ; count++)
+        soma =  soma + count;
+
+    document.write("Soma: "+soma);
+}    
---
+++ somedeumamil/somedeumamil2.js
@@ -0,0 +1,7 @@
+//Crie um programa que some de 1 a 1000 utilizando for
+function somar(){
+    for(var soma=0, count=1 ; count<=1000 ; count++)
+    soma += count;
+
+    document.writeln("Soma: "+soma);
+}
