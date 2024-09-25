a90c3dfd97058315919a0e0b6a34203b38070e30
Germano Ventura <og3rm4n0@gmail.com>
2024-09-25 20:52:33 -0300

    Delete Trabalho Lógica directory

--- Trabalho Lógica/1003.js
+++
@@ -1,4 +0,0 @@
-let A = parseInt(lines.shift(30));
-let B = parseInt(lines.shift(10));
-
-console.log(`SOMA = ${A + B}`);
--- Trabalho Lógica/helloworld.js
+++
@@ -1,6 +0,0 @@
-//O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
-//Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
-let texto1 = "Hello" ; 
-let texto2 = "World" ; 
-let frasecompleta = texto1 + " " + texto2
-console.log(frasecompleta)
