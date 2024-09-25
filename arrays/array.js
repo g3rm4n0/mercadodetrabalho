f6a5ecfa868a9cf51e712585e88b0ff90820f6ee
Germano Ventura <og3rm4n0@gmail.com>
2024-09-25 20:53:04 -0300

    Add files via upload

---
+++ arrays/array.js
@@ -0,0 +1,15 @@
+//Exibir os conteúdos do array:
+//Usando console.log(): Para exibir o array no console do
+//navegador. Usando document.write(): Para exibir o array diretamente
+//na página HTML
+
+let frutas = ["maçã", "banana", "laranja"];
+
+//Exibe no console
+
+console.log(frutas);
+
+//Exibe na página
+
+document.write(frutas);  //Exibe "maçã, banana, laranja"
+
---
+++ arrays/length.js
@@ -0,0 +1,4 @@
+//length: Usado para encontrar o tamanho do array (número de
+//elementos)
+let numeros = [10, 20, 30, 40, 50];
+console.log(numeros.length);
